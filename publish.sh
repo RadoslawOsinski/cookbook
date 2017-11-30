66cd99300cccc38e36b3f2d338054fff0a4126dd
Radosław Osiński <Radoslaw.Osinski@gmail.com>
2017-11-30 18:38:54 +0100

    Eksperyment z dokumentacją.

---
+++ publish.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#clear output dir
+rm -R output;
+
+#publish HTML
+asciidoctor -b html5 -a icons -a toc2 -a theme=flask book/main.adoc -D output/html/;
+rsync -zarv --include="*/" --include="*.jpg" --include="*.jpeg"  --include="*.png" --exclude="*" "book/" "output/html/"
+
+
+#publish dockbook
+asciidoctor -b docbook book/main.adoc -D output/docbook/;
+
+#publish pdf
+asciidoctor -r asciidoctor-pdf -b pdf book/main.adoc -D output/pdf/;
+
+#publish epub
+#asciidoctor -r asciidoctor-epub3 -b epub3 book/main.adoc -D output/epub/;
+#asciidoctor-epub3 book/main.adoc -D  output/ebook/;
